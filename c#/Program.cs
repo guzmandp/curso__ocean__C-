0dd1a38875691d4bfa6a2ee1950e1f5c06ed738b
guzmandp <guzmandp@gmail.com>
2022-02-17 18:42:12 -0300

    first commit

---
+++ c#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            inicio();
+        }
+
+        static void inicio()
+        {
+            // comentario de linea
+
+            /*  comentario de bloque
+             comentario de bloque
+             comentario de bloque */
+
+            int num1 = 2;
+            int num2 = 5;
+            int result = num1 * num2;
+            string hello = "Hello World!";
+            string nome;
+
+            Console.WriteLine(result);
+            Console.WriteLine(hello);
+            Console.WriteLine("Hello World!");
+
+            nome = "Darwin";
+            Console.WriteLine(nome);
+        }
+    }
+}
